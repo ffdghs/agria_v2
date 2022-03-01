72fed34df438af3100444a8c3ec1513775882c59
Nicolas Nielsen <nnielsen.dev@gmail.com>
2022-03-02 00:00:36 +0100

    Update Badge, Habitat, Picture components

--- assets/js/backoffice/Pages/Badge.vue
+++ assets/js/backoffice/Pages/Badge.vue
@@ -1,9 +1,44 @@
 <template>
-    <p>Test component</p>
+    <h2>Badge list</h2>
+    <Table :rows="badges" :config="config">
+        <template #url_badge="{ url_badge }">
+            <img :src="'/build/images/badge/' + url_badge" :alt="url_badge">
+        </template>
+    </Table>
 </template>
 
 <script>
-    export default {
-    name: "test"
+import Table from '../Components/Table'
+export default {
+    name: "badge",
+    components: {
+        Table
+    },
+    data() {
+        return {
+            badges: [],
+            config: {
+                columns: {
+                    "name" : {"data": "name_badge"},
+                    "description" : {"data": "description_badge"},
+                    "picture" : {"data": "url_badge"},
+                },
+                actions: true,
+            }
+        }
+    },
+    methods: {
+    },
+    created() {
+        fetch('/get/badges', {method: 'GET'})
+            .then(response => response.json())
+            .then(response => this.badges = response)
     }
+}
 </script>
+
+<style scoped>
+img {
+    width: 100px;
+}
+</style>
--- assets/js/backoffice/Pages/Habitat.vue
+++ assets/js/backoffice/Pages/Habitat.vue
@@ -1,9 +1,38 @@
 <template>
-    <p>Test component</p>
+    <h2>Habitat list</h2>
+    <Table :rows="habitats" :config="config"></Table>
 </template>
 
 <script>
-    export default {
-    name: "test"
+import Table from '../Components/Table'
+export default {
+    name: "habitat",
+    components: {
+        Table
+    },
+    data() {
+        return {
+            habitats: [],
+            config: {
+                columns: {
+                    "name" : {"data": "name_habitat"},
+                },
+                actions: true,
+            }
+        }
+    },
+    methods: {
+    },
+    created() {
+        fetch('/get/habitats', {method: 'GET'})
+            .then(response => response.json())
+            .then(response => this.habitats = response)
     }
+}
 </script>
+
+<style scoped>
+img {
+    width: 100px;
+}
+</style>
